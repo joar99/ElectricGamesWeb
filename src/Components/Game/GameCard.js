a779cd3533cbd6d636b6d5e22452c24e532ff8cb
KristineMH <77104021+KristineMH@users.noreply.github.com>
2022-11-15 20:47:15 +0100

    read-more link and updated gamecard

--- src/Components/Game/GameCard.js
+++ src/Components/Game/GameCard.js
@@ -15,7 +15,7 @@ export default function GameCard(props) {
                     <section className="information-box">
                         <h1 className="title">{props.title}</h1>
                         <p id="id">{props.id}</p>
-                        <button className="read-more">{props.readMore}</button> Read More
+                        <Link to className="read-more">{props.readMore}Read More</Link>
                     </section>
             <p>{props.releaseDate}</p>
             <Link to={`/games/${props.id}`}>Edit Game</Link>
