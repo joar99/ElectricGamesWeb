f4cca9cffa7884b13cf4591781ebca9ab4ab3f14
KristineMH <77104021+KristineMH@users.noreply.github.com>
2022-11-17 13:13:59 +0100

    working

--- src/Components/Game/AddGame.js
+++ src/Components/Game/AddGame.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import axios from "axios";
 import GameCard from "./GameCard";
 import "../../Css/Default.css";
@@ -30,7 +30,7 @@ export default function AddGame() {
         setFile(e.target.files[0]);
         setFileName(e.target.files[0].name)
     }
-    
+
     const game = {
         id: null,
         Title: title,
@@ -40,7 +40,7 @@ export default function AddGame() {
         Developer: developer,
         ESRB: rating
     }
-    
+
     const postGame = async () => {
         await axios.post(gameControllerUrl,
             JSON.stringify(game),
@@ -49,37 +49,53 @@ export default function AddGame() {
                     "Content-Type": "application/json"
                 }
             })
-            .then((response)=>{
+            .then((response) => {
                 console.log(response)
             })
-            .catch(error=>console.log(error));
+            .catch(error => console.log(error));
 
-            const formData = new FormData();
-            formData.append("formFile", file)
-            formData.append("fileName", fileName)
-    
-            try {
+        const formData = new FormData();
+        formData.append("formFile", file)
+        formData.append("fileName", fileName)
+
+        try {
             const res = axios.post("https://localhost:7127/api/file", formData);
-            } catch (ex) {
+        } catch (ex) {
             console.log(ex)
-            }
         }
+    }
 
     return (
         <>
-            <p>Enter Game Title</p>
-            <input className="game-title-input" type="text" onChange={handleChangeTitle}></input>
-            <p>Enter Game Release Date</p>
-            <input className="game-release-input" type="text" onChange={handleChangeReleaseDate}></input>
-            <p>Enter Game Platform</p>
-            <input className="game-platform-input" type="text" onChange={handleChangePlatform}></input>
-            <p>Enter Game Developer</p>
-            <input className="game-developer-input" type="text" onChange={handleChangeDeveloper}></input>
-            <p>Enter Game Age Rating</p>
-            <input className="game-age-input" type="text" onChange={handleChangeRating}></input>
-            <p>Select Image</p>
-            <input type="file" onChange={saveFile}></input>
-            <button onClick={postGame}>Create Game</button>
+            <section className="background-container">
+                <div className="form-container">
+                    <form className="form-input">
+                        <div>
+                            <label type="text" name="game-title">Enter Game Title</label>
+                            <input className="game-title-input" type="text" onChange={handleChangeTitle}></input>
+                        </div>
+                        <div>
+                            <label type="text" name="release-date">Enter Game Release Date</label>
+                            <input className="game-release-input" type="text" onChange={handleChangeReleaseDate}></input>
+                        </div>
+                        <div>
+                            <label type="text" name="game-platform">Enter Game Platform</label>
+                            <input className="game-platform-input" type="text" onChange={handleChangePlatform}></input>
+                        </div>
+                        <div>
+                            <label type="text" name="game-developer">Enter Game Developer</label>
+                            <input className="game-developer-input" type="text" onChange={handleChangeDeveloper}></input>
+                        </div>
+                        <label type="text" name="game-rating">Enter Game Age Rating</label>
+                        <input className="game-age-input" type="text" onChange={handleChangeRating}></input>
+                        <div>
+                            Select Image
+                            <input type="file" onChange={saveFile}></input>
+                            <button onClick={postGame}>Create Game</button>
+                        </div>
+                    </form>
+                </div>
+            </section>
         </>
     )
 }
--- src/Components/Game/GameCard.js
+++ src/Components/Game/GameCard.js
@@ -5,23 +5,27 @@ import "../../Css/Game/GameCard.css"
 
 export default function GameCard(props) {
 
-    
-    
+
     return (
         <>
-        <section className="container-box-background">
-            <section className="container-box">
-                <img src={`https://localhost:7127/images/${encodeURIComponent(props.image)}`} alt={`https://localhost:7127/images/placeholder.png`} ></img>
-                    <section className="information-box">
-                        <h1 className="title">{props.title}</h1>
-                        <p id="id">{props.id}</p>
-                        <Link to className="read-more">{props.readMore}Read More</Link>
-                    </section>
-            <p>{props.releaseDate}</p>
-            <Link to={`/games/${props.id}`}>Edit Game</Link>
-            
+            <section className="card-container">
+                <div className="card">
+                    <div className="card-image">
+                        <img src={`https://localhost:7127/images/${encodeURIComponent(props.image)}`} alt={`https://localhost:7127/images/placeholder.png`} ></img>
+                    </div>
+                    <div className="card-overlay">
+                        <h1>{props.title}</h1>
+                        <p className="id">{props.id}</p>
+                        <p>Release Date: {props.releaseDate}</p>
+                        <p>Age Limit: {props.esrb}</p>
+                        <p>Platform: {props.platform}</p>
+                        <p>Developer: {props.developer}</p>
+                        <Link className="edit-link" to={`/games/${props.id}`}>Edit Game</Link>
+                        <button className="fake-delete-button">Delete</button>
+                    </div>
+                </div>
             </section>
-        </section>
+
         </>
     )
 
