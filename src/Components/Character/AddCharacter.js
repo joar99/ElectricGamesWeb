0e2e432add0b0e3ea01e09cff47cf2dfefb816c9
KristineMH <77104021+KristineMH@users.noreply.github.com>
2022-11-17 13:40:36 +0100

    updated classname in addcharacters, and important classes for styling

--- src/Components/Character/AddCharacter.js
+++ src/Components/Character/AddCharacter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import axios from "axios";
 import "../../Css/Default.css";
 
@@ -25,7 +25,7 @@ export default function AddCharacter() {
         setFile(e.target.files[0]);
         setFileName(e.target.files[0].name)
     }
-    
+
     const character = {
         id: null,
         Name: name,
@@ -33,7 +33,7 @@ export default function AddCharacter() {
         Image: fileName,
         Weapon: weapon
     }
-    
+
     const postCharacter = async () => {
         await axios.post(charControllerUrl,
             JSON.stringify(character),
@@ -42,32 +42,40 @@ export default function AddCharacter() {
                     "Content-Type": "application/json"
                 }
             })
-            .then((response)=>{
+            .then((response) => {
                 console.log(response)
             })
-            .catch(error=>console.log(error));
+            .catch(error => console.log(error));
 
-            const formData = new FormData();
-            formData.append("formFile", file)
-            formData.append("fileName", fileName)
-    
-            try {
+        const formData = new FormData();
+        formData.append("formFile", file)
+        formData.append("fileName", fileName)
+
+        try {
             const res = axios.post("https://localhost:7127/api/file", formData);
-            } catch (ex) {
+        } catch (ex) {
             console.log(ex)
-            }
         }
+    }
 
     return (
         <>
-            <p>Enter Character Name</p>
-            <input className="game-title-input" type="text" onChange={handleChangeName}></input>
-            <p>Enter Game Character Appears In</p>
-            <input className="game-release-input" type="text" onChange={handleChangeGame}></input>
-            <p>Enter Weapon of Character</p>
-            <input className="game-platform-input" type="text" onChange={handleChangeWeapon}></input>
-            <p>Select Image</p>
-            <input type="file" onChange={saveFile}></input>
+            <div>
+                <label type="text" name="character-name">Enter Character Name</label>
+                <input className="character-name-input" type="text" onChange={handleChangeName}></input>
+            </div>
+            <div>
+                <label type="text" name="character-game">Enter Game Character Appears In</label>
+                <input className="character-game-input" type="text" onChange={handleChangeGame}></input>
+            </div>
+            <div>
+                <label type="text" name="weapon">Enter Weapon of Character</label>
+                <input className="character-weapon-input" type="text" onChange={handleChangeWeapon}></input>
+            </div>
+            <div>
+                <label type="button" name="select-image">Select Image</label>
+                <input type="file" onChange={saveFile}></input>
+            </div>
             <button onClick={postCharacter}>Create Character</button>
         </>
     )
--- src/Components/Character/CharacterCard.js
+++ src/Components/Character/CharacterCard.js
@@ -5,30 +5,31 @@ import DeleteChar from "./DeleteChar";
 import axios from "axios";
 
 
-export default function CharacterCard({character, setDeleteCharacterFlag}) {
+export default function CharacterCard({ character, setDeleteCharacterFlag }) {
 
     const characterControllerUrl = "https://localhost:7127/api/Characters";
 
     const onDeleteCharacter = async (id) => {
         await axios.delete(`${characterControllerUrl}/${id}`)
-        .then((response => {setDeleteCharacterFlag(prev=>!prev)}))
-        .catch(error => console.log(error))
+            .then((response => { setDeleteCharacterFlag(prev => !prev) }))
+            .catch(error => console.log(error))
     }
-    
+
     return (
         <>
-        <section className="container-box-background">
-            <section className="container-box">
-                <img src={`https://localhost:7127/images/${encodeURIComponent(character.image)}`} alt={`https://localhost:7127/images/placeholder.png`} ></img>
-                    <section className="information-box">
-                        <h1 className="title">{character.name}</h1>
-                        <p id="id">{character.id}</p>
-                    </section>
-            <p>{character.game}</p>
-            <Link to={`/games/${character.id}`}>Edit Character</Link>
-            <DeleteChar onDeleteCharacter={onDeleteCharacter} idToDelete={character.id}></DeleteChar>
-            </section>
-        </section>
+            <div className="card">
+                <div className="card-image">
+                    <img src={`https://localhost:7127/images/${encodeURIComponent(character.image)}`} alt={`https://localhost:7127/images/placeholder.png`} ></img>
+                </div>
+                <div className="card-overlay">
+                    <h1>{character.name}</h1>
+                    <p id="id">{character.id}</p>
+                    <p>{character.game}</p>
+                    <Link to={`/games/${character.id}`}>Edit Character</Link>
+                    <DeleteChar onDeleteCharacter={onDeleteCharacter} idToDelete={character.id}></DeleteChar>
+                </div>
+
+            </div>
         </>
     )
 
