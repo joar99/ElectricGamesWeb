e03d5f61e3f59928cc32459df4e17baac97a48b4
KristineMH <77104021+KristineMH@users.noreply.github.com>
2022-11-20 22:01:33 +0100

    done for the day

--- src/Components/Game/SearchGame.js
+++ src/Components/Game/SearchGame.js
@@ -1,21 +1,32 @@
-import {useState} from "react"
+import { useState } from "react"
+import "../../Css/Game/SearchGame.css"
 
-export default function SearchGame({onSearchByTitle}) {
-    
-    const [searchTitle, setSearchTitle] = useState([]);   
+export default function SearchGame({ onSearchByTitle }) {
+
+    const [searchTitle, setSearchTitle] = useState([]);
+    const [showSearch, setShowSearch] = useState(false);
 
     const searchGameByTitle = () => {
         onSearchByTitle(searchTitle);
     }
-    
 
-    return(
-        <>
-            <h3>Search a Game by Title</h3>
-            <div>                
-                <input type="text" id="search-book-name" placeholder="Enter Game Title" onChange={(e)=>setSearchTitle(e.target.value)}></input>
-             </div>
-            <button id="search-book-name-btn" onClick={searchGameByTitle}>Search</button>
-        </>
+
+    return (
+       
+       
+  
+        <section className="search-container">
+              { showSearch ? (
+            <section>
+                <h3 className="search-title">Search a Game by Title</h3>
+
+                <div className="search-pop-up">
+                    <input type="text" id="search-game-name" className="search-game-name" placeholder="Enter Game Title" onChange={(e) => setSearchTitle(e.target.value)}></input>
+                    <button className="search-game-name-btn" onClick={searchGameByTitle}>Search</button>
+                </div>
+            </section>
+              ): <button className="magnify-glass" onClick={()=> setShowSearch(true)}>Search Icon</button>}
+        </section>
+        
     )
 }
--- src/Components/Nav/SideNav.js
+++ src/Components/Nav/SideNav.js
@@ -18,8 +18,6 @@ const SideNav = () => {
 
     )
 
-
-
 }
 
 
