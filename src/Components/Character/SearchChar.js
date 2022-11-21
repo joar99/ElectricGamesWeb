15507c66d324314da8b6e2bc884ae7a9effe320d
KristineMH <77104021+KristineMH@users.noreply.github.com>
2022-11-21 21:23:29 +0100

    fixed padding search-bars char and game

--- src/Components/Character/SearchChar.js
+++ src/Components/Character/SearchChar.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import "../../Css/Character/SearchCharacter.css"
 
 export default function SearchChar({ onSearchByName, onSearchCharById }) {
 
@@ -21,14 +22,14 @@ export default function SearchChar({ onSearchByName, onSearchCharById }) {
 
                     <div className="search-content">
                         <input type="text" id="search-character-name" className="search-character-name" placeholder="Enter Character Name" onChange={(e) => setSearchName(e.target.value)}></input>
-                        <button id="search-character-name-btn" onClick={searchCharByName}>Search</button>
+                        <button id="search-character-name-btn" className="search-character-name-btn" onClick={searchCharByName}>Search</button>
                     </div>
                 </section>
                 <section>
                     <h3 className="search-name">Search a character by ID</h3>
                     <div className="search-content">
                     <input type="text" id="search-character-id" className="search-character-id" placeholder="Enter character ID" onChange={(e) => setSearchCharId(e.target.value)}></input>
-                    <button id="search-character-id" onClick={searchCharById}>Search</button>
+                    <button id="search-character-id-btn" className="search-character-id-btn" onClick={searchCharById}>Search</button>
                     </div>
                 </section>
             </section>
