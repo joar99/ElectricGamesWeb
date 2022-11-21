74f460e804a13ec09127714ad87c55bfa7fec902
KristineMH <77104021+KristineMH@users.noreply.github.com>
2022-11-21 21:32:27 +0100

    characters added search function, missing one though

--- src/Components/Character/Characters.js
+++ src/Components/Character/Characters.js
@@ -1,5 +1,5 @@
 import React, { useMemo } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import useFetch from "../../Hooks/useFetch";
@@ -14,43 +14,45 @@ export default function Characters() {
 
     const [deleteCharacterFlag, setDeleteCharacterFlag] = useState(false);
     const [characters, setCharacters] = useState([]);
-    
+
     const characterControllerUrl = "https://localhost:7127/api/Characters";
 
     useEffect(() => {
         axios.get(characterControllerUrl)
-        .then(response=>setCharacters(response.data))
-        .catch(error=>console.log(error))
-    },[deleteCharacterFlag])
+            .then(response => setCharacters(response.data))
+            .catch(error => console.log(error))
+    }, [deleteCharacterFlag])
 
     const onSearchByName = (searchName) => {
         axios.get(`${characterControllerUrl}/${searchName}`)
-        .then(response=>setCharacters(response.data))
-        .catch(error=>console.log(error))
+            .then(response => setCharacters(response.data))
+            .catch(error => console.log(error))
     }
 
-    
+
 
     return (
 
         <>
-        <div>
-            <Link to={`/games/addcharacter`}>Add New Character</Link>
+
+            <h1 className="main-title">Characters</h1>
             <SearchChar onSearchByName={onSearchByName}></SearchChar>
-        </div>
-        <h1 className="main-title">Characters</h1>
-        <section className="card-container">
-        {characters.map(char =>{
-                return(
-                    <>
-                    <CharacterCard character={char} setDeleteCharacterFlag={setDeleteCharacterFlag}/>
-                    </>
-                )
-            })}
-        </section>
+            <section className="card-container">
+                {characters.map(char => {
+                    return (
+                        <>
+                            <CharacterCard character={char} setDeleteCharacterFlag={setDeleteCharacterFlag} />
+                        </>
+                    )
+                })}
+            </section>
+            <section className="add-new-character">
+                <Link to={`/games/addcharacter`}>Add New Character</Link>
+            </section>
+
         </>
 
-        
+
     )
 
 }
