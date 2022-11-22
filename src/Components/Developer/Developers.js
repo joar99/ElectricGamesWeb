17e46de67d712d1f91f0a98e226ea04e5b95419f
Jonas S. Arnesen <theereal_jonas@outlook.com>
2022-11-22 11:23:33 +0100

    fixedbugs

--- src/Components/Developer/Developers.js
+++ src/Components/Developer/Developers.js
@@ -1,6 +1,6 @@
 import React, { useMemo } from "react";
 import {Link} from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import SearchDev from "./SearchDev";
 import DevCard from "./DevCard"
