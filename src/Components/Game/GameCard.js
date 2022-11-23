c6c203af956f713d4657430c566caa0784a88736
KristineMH <77104021+KristineMH@users.noreply.github.com>
2022-11-23 13:00:35 +0100

    gamecard-fixed

--- src/Components/Game/GameCard.js
+++ src/Components/Game/GameCard.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "../../Css/Game/GameCard.css"
 import DeleteGame from "./DeleteGame";
 import axios from "axios";
+
 
 
 
@@ -10,37 +12,41 @@ export default function GameCard({ game, setDeleteGameFlag }) {
 
     const gameControllerUrl = "https://localhost:7127/api/Games";
 
+    const gamesUrl = "https://localhost/7127/games";
+
+    const [img, setImg] = useState();
+
     const onDeleteGame = async (id) => {
         await axios.delete(`${gameControllerUrl}/${id}`)
             .then((response => { setDeleteGameFlag(prev => !prev) }))
             .catch(error => console.log(error))
     }
 
+    const backgroundLink = `https://localhost:7127/images/${encodeURIComponent(game.image)}`
+
     return (
         <>
-             <div className="card">
-                <div className="card-image">
-                    <img src={`https://localhost:7127/images/${encodeURIComponent(game.image)}`} alt={`https://localhost:7127/images/placeholder.png`} ></img>
-                </div>
-                <div className="card-overlay">
-                    <ul class="card-overlay_list">
-                        <h1>{game.title}</h1>
-                        <li className="id">{game.id}</li>
-                        <li>Release Date: {game.releaseDate}</li>
-                        <li>Age Limit: {game.esrb}</li>
-                        <li>Platform: {game.platform}</li>
-                        <li>Developer: {game.developer}</li>
-                        <ul class="card-overlay-buttons">
-                            <li><Link className="edit-game-link" to={`/games/${game.id}`}>Edit Game</Link></li>
-                            <li><DeleteGame onDeleteGame={onDeleteGame} idToDelete={game.id}></DeleteGame></li>
+            <div className="card" style={{ backgroundImage: `url(${backgroundLink})` }}>
+                    <div className="card-overlay">
+                        <ul class="card-overlay_list">
+                            <h1>{game.title}</h1>
+                            <li className="id">{game.id}</li>
+                            <li>Release Date: {game.releaseDate}</li>
+                            <li>Age Limit: {game.esrb}</li>
+                            <li>Platform: {game.platform}</li>
+                            <li>Developer: {game.developer}</li>
+                            <ul class="card-overlay-buttons">
+                                <li><Link className="edit-game-link" to={`/games/${game.id}`}>Edit Game</Link></li>
+                                <li><DeleteGame onDeleteGame={onDeleteGame} idToDelete={game.id}></DeleteGame></li>
+                            </ul>
                         </ul>
-                    </ul>
+                    </div>
                 </div>
-            </div>
-
+            
 
 
         </>
     )
 
 }
+
