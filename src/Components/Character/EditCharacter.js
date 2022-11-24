97c42d28012a6102e26fe83787fc90fd9188da17
Vicgri <89450050+Vicgri@users.noreply.github.com>
2022-11-24 11:13:17 +0100

    endret editgame js og editchar

--- src/Components/Character/EditCharacter.js
+++ src/Components/Character/EditCharacter.js
@@ -1,168 +1,197 @@
 import React, { useState, Component, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
-import "../../Css/Character/EditCharacter.css";
-import "../../Css/PopupMessage.css"
+import "../../Css/PopupMessage.css";
 import "../../Css/EditForm.css";
 
-
-
-
-
 export default function Character() {
+  //GETTING ID BASED ON URL
+  let { id } = useParams();
 
-    //GETTING ID BASED ON URL
-    let { id } = useParams()
+  //STATE CONTROL FOR POPUP
+  const [popup, setPopup] = useState(false);
+  const handlePopupChange = () => {
+    setPopup(true);
+    setTimeout(() => {
+      setPopup(false);
+    }, 3000);
+  };
+  //BUTTON FOR POPUP
+  const handleClick = (event) => {
+    event.preventDefault();
+    putChar();
+    handlePopupChange();
+  };
 
-    //STATE CONTROL FOR POPUP
-    const [popup, setPopup] = useState(false);
-    const handlePopupChange = () => {
-        setPopup(true)
-        setTimeout(() => {
-            setPopup(false)
-        }, 3000)
+  //STATE CONTROL FOR GAME
+  const [character, setCharacter] = useState({});
+  const [characterUpdate, setCharacterUpdate] = useState(false);
+
+  const characterControllerUrl = "https://localhost:7127/api/Characters";
+
+  //GET GAME TO DISPLAY
+  useEffect(() => {
+    axios
+      .get(`${characterControllerUrl}/${id}`)
+      .then((response) => setCharacter(response.data))
+      .catch((error) => console.log(error));
+  }, [characterUpdate]);
+
+  //CONTROL INPUT BOX STATE
+  const [name, setName] = useState("");
+  const [game, setGame] = useState("");
+  const [weapon, setWeapon] = useState("");
+
+  //STATE CONTROL FILE INPUT
+  const [file, setFile] = useState();
+  const [fileName, setFileName] = useState();
+
+  //HANDLING CHANGE OF STATE FILE INPUT
+  const saveFile = (e) => {
+    setFile(e.target.files[0]);
+    setFileName(e.target.files[0].name);
+  };
+
+  //SETTING NEW GAME TO BE PUT
+  const newChar = {
+    Name: name,
+    Game: game,
+    Image: fileName,
+    Weapon: weapon,
+  };
+
+  //SETTING DEFAULT VALUES FOR GAME TO BE PUT
+  useEffect(() => {
+    setName(character.name);
+    setGame(character.game);
+    setFileName(character.image);
+    setWeapon(character.weapon);
+  }, [character]);
+
+  //POSTING GAME TO DATABASE
+  const putChar = () => {
+    axios
+      .put(`${characterControllerUrl}/${id}`, JSON.stringify(newChar), {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        setCharacterUpdate((prev) => !prev);
+      })
+      .catch((error) => console.log(error));
+
+    const formData = new FormData();
+    formData.append("formFile", file);
+    formData.append("fileName", fileName);
+
+    try {
+      const res = axios.post("https://localhost:7127/api/file", formData);
+    } catch (ex) {
+      console.log(ex);
     }
-    //BUTTON FOR POPUP
-    const handleClick = event => {
-        event.preventDefault();
-        putChar();
-        handlePopupChange();
-    }
+  };
 
-    //STATE CONTROL FOR GAME
-    const [character, setCharacter] = useState({})
-    const [characterUpdate, setCharacterUpdate] = useState(false)
+  return (
+    <>
+      <Link className="go-back-btn" to="/games">
+        <button></button>
+      </Link>
 
-    const characterControllerUrl = "https://localhost:7127/api/Characters";
-
-    //GET GAME TO DISPLAY
-    useEffect(() => {
-        axios.get(`${characterControllerUrl}/${id}`)
-            .then(response => setCharacter(response.data))
-            .catch(error => console.log(error))
-    }, [characterUpdate])
-
-    //CONTROL INPUT BOX STATE
-    const [name, setName] = useState("");
-    const [game, setGame] = useState("");
-    const [weapon, setWeapon] = useState("");
-
-    //STATE CONTROL FILE INPUT
-    const [file, setFile] = useState();
-    const [fileName, setFileName] = useState();
-
-    //HANDLING CHANGE OF STATE FILE INPUT
-    const saveFile = (e) => {
-        setFile(e.target.files[0]);
-        setFileName(e.target.files[0].name)
-    }
-
-    
-
-    //SETTING NEW GAME TO BE PUT
-    const newChar = {
-        Name: name,
-        Game: game,
-        Image: fileName,
-        Weapon: weapon
-    }
-
-    //SETTING DEFAULT VALUES FOR GAME TO BE PUT
-    useEffect(() => {
-        setName(character.name);
-        setGame(character.game);
-        setFileName(character.image)
-        setWeapon(character.weapon);
-    }, [character]);
-
-
-    //POSTING GAME TO DATABASE
-    const putChar = () => {
-
-        axios.put(`${characterControllerUrl}/${id}`,
-            JSON.stringify(newChar),
-            {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
-            .then((response) => {
-                setCharacterUpdate(prev => !prev)
-            })
-            .catch(error => console.log(error));
-
-        const formData = new FormData();
-        formData.append("formFile", file)
-        formData.append("fileName", fileName)
-
-        try {
-            const res = axios.post("https://localhost:7127/api/file", formData);
-        } catch (ex) {
-            console.log(ex)
-        }
-
-
-    }
-
-
-
-
-    return (
-        <>
-        <div className="overlay">
-            <div className="popup-container">
-                <popup className="popup-message">
-             {popup === true ? <h2>Character successfully updated.</h2> : <></>}
-            </popup>    
-         </div>
+      <h1 className="main-title">Characters</h1>
+      <section className="container">
+        <div className="popup-container">
+          <popup className="popup-message">
+            {popup === true ? <h2>Character successfully updated.</h2> : <></>}
+          </popup>
         </div>
-            <h1 className="main-title">Characters</h1>
-            <section className="container">
-                <div className="form">
-                    <form className="form-input">
-                        <div>
-                            <label className="edit-char-label" type="text" name="edit-char-label">Edit character name</label>
-                            <input className="edit-char-input" type="text" defaultValue={character.name} onChange={(e) => setName(e.target.value)} />
-                        </div>
-                        <div>
-                            <label className="edit-char-label" type="text" name="edit-char-label">Edit character game</label>
-                            <input className="edit-char-input" type="text" defaultValue={character.game} onChange={(e) => setGame(e.target.value)} />
-                        </div>
-                        <div>
-                            <label className="edit-char-label" type="text" name="edit-char-label">Edit character weapon</label>
-                            <input className="edit-char-input" type="text" defaultValue={character.weapon} onChange={(e) => setWeapon(e.target.value)} />
-                        </div>
-                        <div>
-                            <label className="edit-char-label" type="text" name="edit-char-label-select">Select Image</label>
-                            <input className="edit-file" type="file" onChange={saveFile} />
-                        </div>
-                        <button className="update-char-btn" onClick={handleClick}>Update Character</button>
-                        <CharacterPreview {...character} />
-                    </form>
-                   
-                </div>
-            </section>
-
-        </>
-    )
-
+        <div className="form">
+          <CharacterPreview {...character} />
+          <form className="form-input">
+            <div>
+              <label
+                className="edit-label"
+                type="text"
+                name="edit-label"
+              >
+                Edit character name
+              </label>
+              <input
+                className="edit-input"
+                type="text"
+                defaultValue={character.name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label
+                className="edit-label"
+                type="text"
+                name="edit-label"
+              >
+                Edit character game
+              </label>
+              <input
+                className="edit-input"
+                type="text"
+                defaultValue={character.game}
+                onChange={(e) => setGame(e.target.value)}
+              />
+            </div>
+            <div>
+              <label
+                className="edit-label"
+                type="text"
+                name="edit-label"
+              >
+                Edit character weapon
+              </label>
+              <input
+                className="edit-input"
+                type="text"
+                defaultValue={character.weapon}
+                onChange={(e) => setWeapon(e.target.value)}
+              />
+            </div>
+            <div>
+              <label
+                className="edit-label"
+                type="text"
+                name="edit-label-select"
+              >
+                Select Image
+              </label>
+              <input className="edit-file" type="file" onChange={saveFile} />
+            </div>
+            <button className="update-button" onClick={handleClick}>
+              Update Character
+            </button>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 }
 
 const CharacterPreview = (props) => {
-    return (
-        <>
-            <div className="preview-container">
-                    <img src={`https://localhost:7127/images/${encodeURIComponent(props.image)}`} alt={`https://localhost:7127/images/placeholder.png`} className={"preview-img"}></img>
-                <div className="card-overlay">
-                    <ul class="card-overlay_list">
-                        <h1>{props.name}</h1>
-                        <li className="id">{props.id}</li>
-                        <li>Game: {props.game}</li>
-                        <li>Weapon: {props.weapon}</li>
-                    </ul>
-                </div>
-            </div>
-
-        </>
-    )
-}
+  const backgroundLink = `https://localhost:7127/images/${encodeURIComponent(
+    props.image
+  )}`;
+  return (
+    <>
+      <div
+        className="small-card"
+        style={{ backgroundImage: `url(${backgroundLink})` }}
+      >
+        <div className="small-card-overlay">
+          <ul class="small-card-overlay_list">
+            <h1>{props.name}</h1>
+            <li className="id">{props.id}</li>
+            <li>Game: {props.game}</li>
+            <li>Weapon: {props.weapon}</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/Components/Game/EditGame.js
+++ src/Components/Game/EditGame.js
@@ -1,187 +1,234 @@
 import React, { useState, Component, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import axios from "axios";
-import "../../Css/Game/EditGame.css";
+import "../../Css/EditForm.css";
 import "../../Css/Background.css";
 import "../../Css/PopupMessage.css";
 
-
-
 export default function Game() {
+  //GETTING ID BASED ON URL
+  let { id } = useParams();
 
-    //GETTING ID BASED ON URL
-    let { id } = useParams()
+  //STATE CONTROL FOR POPUP
+  const [popup, setPopup] = useState(false);
+  const handlePopupChange = () => {
+    setPopup(true);
+    setTimeout(() => {
+      setPopup(false);
+    }, 3000);
+  };
 
-    //STATE CONTROL FOR POPUP
-    const [popup, setPopup] = useState(false);
-    const handlePopupChange = () => {
-        setPopup(true)
-        setTimeout(() => {
-            setPopup(false)
-        }, 3000)
+  //STATE CONTROL FOR GAME
+  const [game, setGame] = useState({});
+  const [gameUpdate, setGameUpdate] = useState(false);
+
+  const gameControllerUrl = "https://localhost:7127/api/Games";
+
+  //GET GAME TO DISPLAY
+  useEffect(() => {
+    axios
+      .get(`${gameControllerUrl}/${id}`)
+      .then((response) => setGame(response.data))
+      .catch((error) => console.log(error));
+  }, [gameUpdate]);
+
+  //CONTROL INPUT BOX STATE
+
+  const [title, setTitle] = useState("");
+  const [releaseDate, setReleaseDate] = useState("");
+  const [platform, setPlatform] = useState("");
+  const [developer, setDeveloper] = useState("");
+  const [rating, setRating] = useState("");
+
+  //STATE CONTROL FILE INPUT
+  const [file, setFile] = useState();
+  const [fileName, setFileName] = useState();
+
+  //HANDLING CHANGE OF STATE FILE INPUT
+  const saveFile = (e) => {
+    setFile(e.target.files[0]);
+    setFileName(e.target.files[0].name);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    putGame();
+    handlePopupChange();
+  };
+
+  //SETTING NEW GAME TO BE PUT
+  const newGame = {
+    Title: title,
+    ReleaseDate: releaseDate,
+    Image: fileName,
+    Platform: platform,
+    Developer: developer,
+    ESRB: rating,
+  };
+
+  //SETTING DEFAULT VALUES FOR GAME TO BE PUT
+  useEffect(() => {
+    setTitle(game.title);
+    setReleaseDate(game.releaseDate);
+    setFileName(game.image);
+    setPlatform(game.platform);
+    setDeveloper(game.developer);
+    setRating(game.esrb);
+  }, [game]);
+
+  //POSTING GAME TO DATABASE
+  const putGame = () => {
+    axios
+      .put(`${gameControllerUrl}/${id}`, JSON.stringify(newGame), {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        setGameUpdate((prev) => !prev);
+      })
+      .catch((error) => console.log(error));
+
+    const formData = new FormData();
+    formData.append("formFile", file);
+    formData.append("fileName", fileName);
+
+    try {
+      const res = axios.post(
+        "https://localhost:7127/api/file",
+        JSON.stringify(Object.fromEntries(formData))
+      );
+    } catch (ex) {
+      console.log(ex);
     }
+  };
 
-    //STATE CONTROL FOR GAME
-    const [game, setGame] = useState({})
-    const [gameUpdate, setGameUpdate] = useState(false)
+  return (
+    <>
+      <Link className="go-back-btn" to="/games">
+        <button></button>
+      </Link>
+      <h1 className="main-title">Games</h1>
+      <section className="container">
+        <div className="popup-container">
+          <popup className="popup-message">
+            {popup === true ? <h2>Game successfully updated.</h2> : <></>}
+          </popup>
+        </div>
 
-    const gameControllerUrl = "https://localhost:7127/api/Games";
+        <div className="form">
+          <GamePreview {...game} />
 
-    //GET GAME TO DISPLAY
-    useEffect(() => {
-        axios.get(`${gameControllerUrl}/${id}`)
-            .then(response => setGame(response.data))
-            .catch(error => console.log(error))
-    }, [gameUpdate])
-
-    //CONTROL INPUT BOX STATE
-
-    const [title, setTitle] = useState("");
-    const [releaseDate, setReleaseDate] = useState("");
-    const [platform, setPlatform] = useState("");
-    const [developer, setDeveloper] = useState("");
-    const [rating, setRating] = useState("");
-
-    //STATE CONTROL FILE INPUT
-    const [file, setFile] = useState();
-    const [fileName, setFileName] = useState();
-
-    //HANDLING CHANGE OF STATE FILE INPUT
-    const saveFile = (e) => {
-        setFile(e.target.files[0]);
-        setFileName(e.target.files[0].name)
-    }
-
-    const handleClick = event => {
-        event.preventDefault();
-        putGame();
-        handlePopupChange();
-    }
-
-    //SETTING NEW GAME TO BE PUT
-    const newGame = {
-        Title: title,
-        ReleaseDate: releaseDate,
-        Image: fileName,
-        Platform: platform,
-        Developer: developer,
-        ESRB: rating
-    }
-
-    //SETTING DEFAULT VALUES FOR GAME TO BE PUT
-    useEffect(() => {
-        setTitle(game.title);
-        setReleaseDate(game.releaseDate);
-        setFileName(game.image)
-        setPlatform(game.platform);
-        setDeveloper(game.developer);
-        setRating(game.esrb);
-    }, [game]);
-
-
-    //POSTING GAME TO DATABASE
-    const putGame = () => {
-        axios.put(`${gameControllerUrl}/${id}`,
-            JSON.stringify(newGame),
-            {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
-            .then((response) => {
-                setGameUpdate(prev => !prev)
-            })
-            .catch(error => console.log(error));
-
-        const formData = new FormData();
-        formData.append("formFile", file)
-        formData.append("fileName", fileName)
-
-        try {
-            const res = axios.post("https://localhost:7127/api/file", JSON.stringify(Object.fromEntries(formData)));
-        } catch (ex) {
-            console.log(ex)
-        }
-    }
-
-
-
-
-    return (
-
-        <>
-
-            <Link className="go-back-btn" to="/games">
-                <button>
-
-                </button>
-            </Link>
-            <h1 className="main-title">Games</h1>
-            <section className="container">
-                {popup === true ? <h1>Game Successfully Updated</h1> : <></>}
-                <div className="form">
-                    <GamePreview {...game} />
-
-                    <form className="form-input">
-                        <div>
-                            <label className="edit-label" type="text" name="edit-game-label">Enter game title</label>
-                            <input className="edit-input" type="text" name="title" value={title} onChange={e => setTitle(e.target.value)} />
-                        </div>
-                        <div>
-                            <label className="edit-label" type="text" name="edit-release-date">Enter Game Release Date</label>
-                            <input className="edit-input" type="text" value={releaseDate} name="releaseDate" onChange={e => setReleaseDate(e.target.value)} />
-                        </div>
-                        <div>
-                            <label className="edit-label" type="text" name="edit-platform">Enter Game Platform</label>
-                            <input className="edit-input" type="text" value={platform} name="platform" onChange={e => setPlatform(e.target.value)} />
-                        </div>
-                        <div>
-                            <label className="edit-label" type="text" name="edit-developer">Enter Game Developer</label>
-                            <input className="edit-input" type="text" value={developer} name="developer" onChange={e => setDeveloper(e.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <label className="edit-label" type="text" name="edit-rating">Enter Game Age Rating</label>
-                            <input className="edit-input" type="text" value={rating} name="rating" onChange={e => setRating(e.target.value)}
-                            />
-                        </div>
-                        <div>
-                            {/* <label htmlFor={'upload-button'}>
+          <form className="form-input">
+            <div>
+              <label className="edit-label" type="text" name="edit-label">
+                Enter game title
+              </label>
+              <input
+                className="edit-input"
+                type="text"
+                name="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div>
+              <label
+                className="edit-label"
+                type="text"
+                name="edit-release-date"
+              >
+                Enter Game Release Date
+              </label>
+              <input
+                className="edit-input"
+                type="text"
+                value={releaseDate}
+                name="releaseDate"
+                onChange={(e) => setReleaseDate(e.target.value)}
+              />
+            </div>
+            <div>
+              <label className="edit-label" type="text" name="edit-platform">
+                Enter Game Platform
+              </label>
+              <input
+                className="edit-input"
+                type="text"
+                value={platform}
+                name="platform"
+                onChange={(e) => setPlatform(e.target.value)}
+              />
+            </div>
+            <div>
+              <label className="edit-label" type="text" name="edit-developer">
+                Enter Game Developer
+              </label>
+              <input
+                className="edit-input"
+                type="text"
+                value={developer}
+                name="developer"
+                onChange={(e) => setDeveloper(e.target.value)}
+              />
+            </div>
+            <div>
+              <label className="edit-label" type="text" name="edit-rating">
+                Enter Game Age Rating
+              </label>
+              <input
+                className="edit-input"
+                type="text"
+                value={rating}
+                name="rating"
+                onChange={(e) => setRating(e.target.value)}
+              />
+            </div>
+            <div>
+              {/* <label htmlFor={'upload-button'}>
                                 <div className="choose-file">
                                     {/*<SomeIconElement style={{ marginRight: 10 }} /> Upload File*/
-
-                            /* <label className="edit-game-label" type="text" name="edit-game-label-select">Select Image</label> */}
-                            <input className="edit-file" type="file" onChange={saveFile} id="upload-button" style={{ display: 'none' }} />
-                        </div>
-                        <button className="update-button" onClick={handleClick}>Edit Game</button>
-                    </form>
-
-                </div>
-            </section>
-        </>
-
-    )
-
+              /* <label className="edit-game-label" type="text" name="edit-game-label-select">Select Image</label> */}
+              <input
+                className="edit-file"
+                type="file"
+                onChange={saveFile}
+                id="upload-button"
+                style={{ display: "none" }}
+              />
+            </div>
+            <button className="update-button" onClick={handleClick}>
+              Edit Game
+            </button>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 }
-
 
 const GamePreview = (props) => {
-    const backgroundLink = `https://localhost:7127/images/${encodeURIComponent(props.image)}`
+  const backgroundLink = `https://localhost:7127/images/${encodeURIComponent(
+    props.image
+  )}`;
 
-    return (
-        <>
-            <div className="small-card" style={{ backgroundImage: `url(${backgroundLink})` }}>
-                <div className="small-card-overlay">
-                    <ul class="small-card-overlay_list">
-                        <h1>{props.title}</h1>
-                        <li className="id">{props.id}</li>
-                        <li>Release Date: {props.releaseDate}</li>
-                        <li>Age Limit: {props.esrb}</li>
-                        <li>Platform: {props.platform}</li>
-                        <li>Developer: {props.developer}</li>
-                    </ul>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div
+        className="small-card"
+        style={{ backgroundImage: `url(${backgroundLink})` }}
+      >
+        <div className="small-card-overlay">
+          <ul class="small-card-overlay_list">
+            <h1>{props.title}</h1>
+            <li className="id">{props.id}</li>
+            <li>Release Date: {props.releaseDate}</li>
+            <li>Age Limit: {props.esrb}</li>
+            <li>Platform: {props.platform}</li>
+            <li>Developer: {props.developer}</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
